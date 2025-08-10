b143855ba12a07118bf0ebab98d44bea0f02558b
daint-vacom <daint@vacom.com.vn>
2025-08-10 13:42:45 +0700

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/VacomTrialForm',
+  base: '/VacomTrialForm/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
